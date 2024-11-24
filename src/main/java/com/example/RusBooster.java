fa7c800a071dcb094d7fd015bf2ee4934fda36c9
Votline <quogg2@gmail.com>
2024-11-24 17:03:18 +0300

    Update bot token retrieval from token.txt

--- src/main/java/com/example/RusBooster.java
+++ src/main/java/com/example/RusBooster.java
@@ -1,4 +1,5 @@
 import java.util.List;
+import java.nio.file.*;
 
 import org.telegram.telegrambots.meta.api.objects.User;
 import org.telegram.telegrambots.meta.api.objects.Update;
@@ -9,7 +10,7 @@ import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
 
 public class RusBooster extends TelegramLongPollingBot{
-	private static final String BOT_TOKEN = "8014602670:AAEngH6WlD_w26GMKiv8QAIZqWYmYIxx-_k";
+	private static final String BOT_TOKEN = loadToken();
 	private static final String BOT_NAME = "RusBooster";
 
 	ReplyKeyboard botMenu = new ReplyKeyboard();
@@ -44,4 +45,11 @@ public class RusBooster extends TelegramLongPollingBot{
 			botMenu.createMenu(this, update.getMessage());
 		}
 	}
+	private static String loadToken() {
+        try {
+            return Files.readString(Path.of("token.txt")).trim();
+        } catch (Exception e) {
+            throw new RuntimeException("Ошибка загрузки токена из файла", e);
+        }
+    }
 }
